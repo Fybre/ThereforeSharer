7e5f91ca4c714c3f9a743a931e5080e01189d3b9
Fybre <craig@fybre.me>
2026-02-07 19:01:49 +1100

    Fix: Tighten spacing and increase window height for Windows DPI scaling

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 	err := wails.Run(&options.App{
 		Title:         "ThereforeSharer",
 		Width:         480,
-		Height:        630,
+		Height:        660,
 		DisableResize: true,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
